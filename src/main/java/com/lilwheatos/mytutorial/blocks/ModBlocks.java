944f34343fc999f734af043928e64d27666ab527
lilwheatos <lilwheatos@gmail.com>
2020-01-28 22:49:44 -0600

    test commit

---
+++ src/main/java/com/lilwheatos/mytutorial/blocks/ModBlocks.java
@@ -0,0 +1,8 @@
+package com.lilwheatos.mytutorial.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+    @ObjectHolder("mytutorial:firstblock")
+    public static FirstBlock FIRSTBLOCK;
+}
---
+++ src/main/java/com/lilwheatos/mytutorial/setup/IProxy.java
@@ -0,0 +1,7 @@
+package com.lilwheatos.mytutorial.setup;
+
+import net.minecraft.world.World;
+
+public interface IProxy {
+    World getClientWorld();
+}
